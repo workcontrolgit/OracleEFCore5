49239f5475bc13a17e361fb377b03051baba05b4
Fuji <fuji.nguyen@workcontrol.com>
2021-03-12 10:10:56 -0500

    Clean code for pub

--- OracleEFCore5/OracleEFCore5.WebApi/Startup.cs
+++ OracleEFCore5/OracleEFCore5.WebApi/Startup.cs
@@ -53,7 +53,7 @@ namespace OracleEFCore5.WebApi
                 app.UseHsts();
             }
 
-            dbContext.Database.EnsureCreated();
+            //dbContext.Database.EnsureCreated();
 
             // Add this line; you'll need `using Serilog;` up the top, too
             app.UseSerilogRequestLogging();
